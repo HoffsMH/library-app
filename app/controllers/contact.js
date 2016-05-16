88f5479b87e6285458c006aa72aa89d9722ee2fa
HoffsMH <hecker.matthew@gmail.com>
2016-05-16 10:43:59 -0400

    adds logic for contact form behavior

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,4 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  emailAddress: '',
+  message: '',
+  responseMessage: '',
+
+  validEmail: Ember.computed('emailAddress', function() {
+    return this.get('emailAddress').match(/^.+@.+\..+$/);
+  }),
+  validMessage: Ember.computed.gte('messageCharCount', 5),
+  validForm: Ember.computed.and('validEmail' , 'validMessage'),
+
+  messageCharCount: Ember.computed('message', function() {
+    return this.get('message').length;
+  }),
+
+
+  isDisabled: Ember.computed.not('validForm'),
+
+  actions: {
+    saveContact() {
+      alert(`Sending your message`);
+      this.set('responseMessage', `We got your message and we'll get in touch soon.`);
+      this.set('emailAddress', '');
+      this.set('message', '');
+    }
+  }
 });
