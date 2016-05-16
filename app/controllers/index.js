6629664404f8cff1eab04feee49368e30a682fcf
HoffsMH <hecker.matthew@gmail.com>
2016-05-16 15:28:00 -0400

    generates admin/invitations route

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -13,11 +13,16 @@ export default Ember.Controller.extend({
     saveInvitation() {
       const email = this.get('emailAddress');
 
-      const newInvitation = this.store.createRecord('invitation', { email: email });
-      newInvitation.save();
+      const newInvitation = this.store.createRecord('invitation', {
+        email: email
+      });
 
-      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
-      this.set('emailAddress', '');
+      newInvitation.save().then((response) => {
+        this.set('responseMessage', `Thank you! We saved your email address: ${response.get('email')}`);
+        this.set('emailAddress', '');
+      });
+
     }
   }
+
 });
